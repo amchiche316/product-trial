1326fd8c4e246be72d5142d3c517a1c8dbc2534d
Sam HAM <harchou2007@hotmail.fr>
2025-09-19 20:52:32 +0200

    Partie02:  missing files

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./shared/features/home/home.component";
+import { RouterModule } from "@angular/router";
 
 export const APP_ROUTES: Routes = [
   {
@@ -12,4 +13,7 @@ export const APP_ROUTES: Routes = [
       import("./products/products.routes").then((m) => m.PRODUCTS_ROUTES)
   },
   { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path:"contact", loadComponent: () => import('./customers/contact/contact.component').then(m=> m.ContactComponent)
+  }
 ];
